3a7c354e056b090820af0ea8a0b5854d96da9b8e
luan <luan.d@curro.co.za>
2022-03-31 18:02:20 +0200

    init commit

---
+++ MyFrameWork/src/test/java/frameWorkClasses/BasePage.java
@@ -0,0 +1,33 @@
+package frameWorkClasses;
+
+public class BasePage {
+
+	//declare the WebDriver
+	
+	
+	// constructor of the base class
+	
+	// method to read the config
+	
+	// wait for element
+	
+	// wait for click
+	
+	// get element text
+	
+	// click element
+	
+	// check if element exists
+	
+	// get element
+	
+	// clean up
+	
+	// enter text
+	
+	// clear text
+	
+	// select from drop-down
+	
+	
+}
